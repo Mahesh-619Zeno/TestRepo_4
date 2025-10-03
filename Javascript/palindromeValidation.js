6262d7aabc68992f2bfdc9a0809210c2414cb502
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-03 14:14:28 +0530

    removed files after configuration of cs

--- Javascript/palindromeValidation.js
+++
@@ -1,6 +0,0 @@
-function isPalindrome(str) {
-    return str === str.split('').reverse().join('');
-}
-
-let str = prompt("Enter a string:");
-alert(isPalindrome(str) ? "Palindrome" : "Not a palindrome");
