6db3c8d1b6dfa2126c180a3308cc32981dabad35
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-10 21:30:14 +0530

    resolved critical once

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
-def add(x, y): return x + y
+def add(num1, num2): return num1 + num2
 
 
 print("Select operation: 1.Add 2.Subtract 3.Multiply 4.Divide")
 choice = input("Enter choice (1/2/3/4): ")
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 if choice == '1':
-    print(f"Result: {add(x, y)}")
+    print(f"Result: {add(num1, num2)}")
 else:
     print("Invalid input")
